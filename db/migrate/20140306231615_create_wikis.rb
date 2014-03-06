0f894d7c0c29a381bbda7ec7b65aec604fbb50f3
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-03-06 18:20:03 -0500

    Create Wiki model

---
+++ db/migrate/20140306231615_create_wikis.rb
@@ -0,0 +1,12 @@
+class CreateWikis < ActiveRecord::Migration
+  def change
+    create_table :wikis do |t|
+      t.string :title
+      t.text :description
+      t.text :body
+      t.boolean :public
+
+      t.timestamps
+    end
+  end
+end
