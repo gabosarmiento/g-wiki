e2b6a60015e4098f7e2a8bec64b752fe7fecf26d
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-03-10 09:24:04 -0500

    Validate wikis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+
+  def control_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'control-group error'
+    else
+      content_tag :div, capture(&block), class: 'control-group'
+    end
+  end
+  
   def markdown(text)
     renderer = Redcarpet::Render::HTML.new
     extensions = {fenced_code_blocks: true}
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -10,7 +10,10 @@ class Wiki < ActiveRecord::Base
   # def should_generate_new_friendly_id?
   #   new_record?
   # end
-
+  validates :wikiname, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :user, presence: true  
   private
   def set_visibility
    self.public = 'true'
