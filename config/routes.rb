c133473230179694fbbd8b3bf795e9f592d9c0df
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-03-13 02:44:48 -0500

    Fix routes because mistype

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
-
-
-  get "users/update"
+GWiki::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
   resources :users, only: [:show]
   resources :wikis
+
   resources :charges, only: [:new, :create]
 
   match "about" => 'welcome#about', via: :get
