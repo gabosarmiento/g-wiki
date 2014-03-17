627bcee0e99ff663b55352d34f94ef1b10fb1898
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-03-17 04:35:41 -0500

    Set in production config_eager_load to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ GWiki::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
+  config.eager_load = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
